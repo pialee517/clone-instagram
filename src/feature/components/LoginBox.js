47b28d6fbf22a5156056f56ab0bf90599607a9c8
Pia <lee240you@gmail.com>
2023-08-03 22:41:01 -0400

    initial

---
+++ src/feature/components/LoginBox.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react'
+import CardHeader from '@mui/material/CardHeader'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Card from '@mui/material/Card'
+import CardMedia from '@mui/material/CardMedia'
+
+const LoginBox = () => {
+  return (
+    <Card>
+        <CardMedia title="" image="" />
+    </Card>
+  )
+}
+
+export default LoginBox
